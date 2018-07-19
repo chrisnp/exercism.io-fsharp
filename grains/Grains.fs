9282a612facf718dc9a20d50cc04add78c3c9ecd
Invigo <invigo.devel@gmail.com>
2018-07-19 16:29:03 +0300

    grains

---
+++ grains/Grains.fs
@@ -0,0 +1,12 @@
+﻿module Grains
+
+let square (n: int): Result<uint64,string> = 
+    match n with 
+    | _ when n <= 0 || n > 64 -> Error "Invalid input"
+    | _ ->  ((pown 2.0 (n - 1)) |> uint64) |> Ok
+
+let total: Result<uint64,string> = 
+    [| 1..64 |] 
+    |> Seq.sumBy (fun x -> pown 2.0 (x - 1) |> uint64)  
+    |> Ok
+ 
