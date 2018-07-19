8818d90d35f68c20897e100a81a6e906ff8acb0f
Invigo <invigo.devel@gmail.com>
2018-07-20 01:21:30 +0300

    sum-of-multiples

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,7 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int = 
+    [1 .. upperBound - 1]
+    |> Seq.filter (fun possible -> 
+                    numbers |> Seq.exists (fun x -> possible % x = 0))
+    |> Seq.sum
