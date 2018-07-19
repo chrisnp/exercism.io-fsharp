db494ad3a1d55cc6a7279367cb6978d63bba6723
Invigo <invigo.devel@gmail.com>
2018-07-20 01:43:24 +0300

    collatz-conjecture

---
+++ collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,13 @@
+﻿module CollatzConjecture
+
+let rec collatzConverge(x: int, length: int) = 
+    match x with
+    | _ when x < 1 -> None
+    | _ when x = 1 -> Some length
+    | _ when x % 2 = 0 -> collatzConverge (x / 2, length + 1)
+    | _ -> collatzConverge (3 * x + 1, length + 1 )
+
+let steps (number: int): int option = 
+    collatzConverge (number, 0)
+
+
