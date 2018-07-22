7a3a17232822a44d62176532791f27ce4aa97774
Invigo <invigo.devel@gmail.com>
2018-07-23 00:50:19 +0300

    protein-translation

---
+++ protein-translation/ProteinTranslation.fs
@@ -0,0 +1,22 @@
+﻿module ProteinTranslation
+
+open System
+
+let translateCodon = function
+  | "AUG" -> "Methionine"
+  | "UUU" | "UUC" -> "Phenylalanine"
+  | "UUA" | "UUG" -> "Leucine"
+  | "UCU" | "UCC" | "UCA" | "UCG" -> "Serine"
+  | "UAU" | "UAC" -> "Tyrosine"
+  | "UGU" | "UGC" -> "Cysteine"
+  | "UGG" -> "Tryptophan"
+  | "UAA" | "UAG" | "UGA" -> "STOP"
+  | other -> failwithf "Not valid codon: %s" other
+
+let proteins rna = 
+    let stopped cod = cod = "STOP"
+    rna 
+    |> Seq.chunkBySize 3 
+    |> List.ofSeq 
+    |> List.map (String >> translateCodon) 
+    |> List.takeWhile (not << stopped) 
