b2f4c81e2235bc2499c89ff8184981f15ecf20c8
Invigo <invigo.devel@gmail.com>
2018-07-24 02:08:14 +0300

    refactor grains

--- grains/Grains.fs
+++ grains/Grains.fs
@@ -1,12 +1,14 @@
 ﻿module Grains
 
+open System.Numerics
+
 let square (n: int): Result<uint64,string> = 
     match n with 
     | _ when n <= 0 || n > 64 -> Error "Invalid input"
-    | _ ->  ((pown 2.0 (n - 1)) |> uint64) |> Ok
+    | _ ->  (2I ** (n - 1) |> uint64) |> Ok
 
 let total: Result<uint64,string> = 
     [| 1..64 |] 
-    |> Seq.sumBy (fun x -> pown 2.0 (x - 1) |> uint64)  
+    |> Seq.sumBy (fun x -> 2I ** (x - 1) |> uint64)  
     |> Ok
  
