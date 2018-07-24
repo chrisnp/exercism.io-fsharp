64d97913a2036111c9b4f92dd78e2f74d9b75424
Invigo <invigo.devel@gmail.com>
2018-07-25 00:56:04 +0300

    isogram

---
+++ isogram/Isogram.fs
@@ -0,0 +1,7 @@
+﻿module Isogram
+
+open System
+let isIsogram (str: string) =
+    let normal = str.ToLowerInvariant() |> Seq.filter System.Char.IsLetterOrDigit 
+    normal
+    |> Set.ofSeq |> Set.count = Seq.length normal       
