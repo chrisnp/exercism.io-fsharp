17b542bbbbb7e67f2ceb424aa7a8d12764708a41
Invigo <invigo.devel@gmail.com>
2018-07-25 03:38:07 +0300

    refactored collatz-conjecture

--- collatz-conjecture/CollatzConjecture.fs
+++ collatz-conjecture/CollatzConjecture.fs
@@ -1,13 +1,13 @@
 ﻿module CollatzConjecture
 
-let rec collatzConverge(x: int, length: int) = 
+let rec collatzConverge (length: int, x: int) = 
     match x with
     | _ when x < 1 -> None
     | _ when x = 1 -> Some length
-    | _ when x % 2 = 0 -> collatzConverge (x / 2, length + 1)
-    | _ -> collatzConverge (3 * x + 1, length + 1 )
+    | _ when x % 2 = 0 -> collatzConverge (length + 1, (x / 2))
+    | _ -> collatzConverge (length + 1, (3 * x + 1 ))
 
 let steps (number: int): int option = 
-    collatzConverge (number, 0)
+    collatzConverge (0, number)
 
 
