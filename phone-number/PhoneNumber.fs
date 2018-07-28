c99c2b022489accd9b5af208cfd87eb9814c031e
Invigo <invigo.devel@gmail.com>
2018-07-28 23:51:15 +0300

    phone-number

---
+++ phone-number/PhoneNumber.fs
@@ -0,0 +1,13 @@
+﻿module PhoneNumber
+
+open System 
+let clean (input : string) : string option  = 
+    let valid ch = ch <> '0' && ch <> '1'
+    let digits = input |> List.ofSeq |> List.filter Char.IsDigit
+    match (digits.Length, digits) with
+    | 11, '1':: num 
+    | 10, num when (valid num.[0]) && (valid num.[3]) ->
+        num
+        |> (Array.ofList >> String) 
+        |> Some
+    | _ -> None  
