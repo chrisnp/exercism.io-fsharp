acaad73600333719b0258f5f78bde903b7836cfa
Invigo <invigo.devel@gmail.com>
2018-08-18 14:06:53 +0300

    triangle passing all tests

--- triangle/Triangle.fs
+++ triangle/Triangle.fs
@@ -1,7 +1,17 @@
 ﻿module Triangle
 
-let equilateral triangle = failwith "You need to implement this function."
 
-let isosceles triangle = failwith "You need to implement this function."
+let private isATriangle triangle =
+    let nonZero = List.sum triangle <> 0.0
+    let inequality =
+        let [a; b; c] = triangle
+        a + b >= c && a + c >= b && b + c >= a
+    inequality && nonZero
 
-let scalene triangle = failwith "You need to implement this function."
+let private distinctSides triangle = triangle |> List.distinct |> List.length
+
+let equilateral triangle = (triangle |> isATriangle) && (triangle |> distinctSides = 1)
+
+let isosceles triangle = (triangle |> isATriangle) && (triangle |> distinctSides <= 2)
+
+let scalene triangle = (triangle |> isATriangle) && (triangle |> distinctSides = 3)
