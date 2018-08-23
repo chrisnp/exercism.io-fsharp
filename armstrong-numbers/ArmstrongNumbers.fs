a46fed8dbead79824b06804cb9a45d1cef61a3c3
Invigo <invigo.devel@gmail.com>
2018-08-23 18:07:10 +0300

    armstrong-numbers

---
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,9 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber (number: int): bool = 
+    let digits = [for ch in string number -> int (ch.ToString())]
+    let len = digits.Length
+    digits
+    |> List.sumBy (fun x -> int (float x ** float len))
+    |> (=) number
+
