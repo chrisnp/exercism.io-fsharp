c5b5307164b5a32816c542d95f5c81880136ef63
Invigo <invigo.devel@gmail.com>
2018-09-04 19:18:54 +0300

    perfect-numbers all tests passing

--- perfect-numbers/PerfectNumbers.fs
+++ perfect-numbers/PerfectNumbers.fs
@@ -2,4 +2,11 @@
 
 type Classification = Perfect | Abundant | Deficient 
 
-let classify n : Classification option = failwith "You need to implement this function."
+let classify n : Classification option = 
+    if n <= 0 then None
+    else
+        let aliquotSum = [1 .. n/2] |> List.filter (fun i -> n % i = 0) |> List.sum 
+        match aliquotSum with 
+        | _ when aliquotSum < n -> Some Classification.Deficient
+        | _ when aliquotSum > n -> Some Classification.Abundant
+        | _ -> Some Classification.Perfect
