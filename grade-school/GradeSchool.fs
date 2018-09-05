06ff113dbc5f88df9dff6ed0f997507a2b9f2937
Invigo <invigo.devel@gmail.com>
2018-09-05 16:57:47 +0300

    grade-school

---
+++ grade-school/GradeSchool.fs
@@ -0,0 +1,18 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty : School = Map.empty
+
+let grade (number: int) (school: School): string list = 
+    match school |> Map.tryFind number with
+    | Some students -> students
+    | None -> []
+let add (student: string) (grd: int) (school: School): School = 
+    let current = school |> grade grd
+    school |> Map.add grd ((student :: current) |> List.sort)
+
+let roster (school: School): (int * string list) list = 
+    school |> Map.toList |> List.sortBy fst
+
+
