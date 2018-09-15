7bfd6dc1f0315f6f0b2b46b85d4cab77ae079817
Invigo <invigo.devel@gmail.com>
2018-09-15 04:21:48 +0300

    beer-song refactor

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -2,12 +2,16 @@
 
 let private stanza num = 
     match num with
-    | 0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    | 1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    | 2 -> "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    | _ -> sprintf "%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.\n" num num (num-1)
+    | 0 -> ["No more bottles of beer on the wall, no more bottles of beer."; 
+            "Go to the store and buy some more, 99 bottles of beer on the wall."]
+    | 1 -> ["1 bottle of beer on the wall, 1 bottle of beer."; 
+            "Take it down and pass it around, no more bottles of beer on the wall."]
+    | 2 -> ["2 bottles of beer on the wall, 2 bottles of beer."; 
+            "Take one down and pass it around, 1 bottle of beer on the wall."]
+    | _ -> [sprintf "%d bottles of beer on the wall, %d bottles of beer." num num;
+            sprintf "Take one down and pass it around, %d bottles of beer on the wall." (num-1)]
 
 let recite (startBottles: int) (takeDown: int) = 
-    [startBottles .. -1 .. startBottles - takeDown] 
-    |> List.map (fun i -> stanza i + "\n") 
-    |> List.reduce (+)
+    [startBottles .. -1 .. startBottles - takeDown + 1] 
+    |> List.map (fun i -> stanza i ) 
+    |> List.reduce (fun x y -> x @ [""] @ y)
