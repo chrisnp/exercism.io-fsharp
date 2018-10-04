4651bd3e7493ab5e2a892ad8fa2c2c61d43df7c0
Invigo <invigo.devel@gmail.com>
2018-10-04 20:21:52 +0300

    nucloetide-counts

--- nucleotide-count/NucleotideCount.fs
+++ nucleotide-count/NucleotideCount.fs
@@ -1,3 +1,4 @@
 ﻿module NucleotideCount
 
-let nucleotideCounts (strand: string): Option<Map<char, int>> =  failwith "You need to implement this function."
+let nucleotideCounts (strand: string): Option<Map<char, int>> =  
+    failwith "You need to implement this function."
