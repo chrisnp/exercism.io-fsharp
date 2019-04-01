12639e13c490784c7b20b41324a62c78f72e4640
Invigo <invigo.devel@gmail.com>
2019-04-01 03:08:23 +0300

    sieve solution

---
+++ sieve/Sieve.fs
@@ -0,0 +1,7 @@
+﻿module Sieve
+
+let rec sieve primes = function
+    | head::tail -> sieve (head::primes) (tail |> List.filter (fun curr -> curr % head > 0)) 
+    | [] -> primes |> List.rev
+
+let primes limit = sieve [] [2..limit]
