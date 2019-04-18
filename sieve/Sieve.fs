efd23d785cee23f8e334b23fb7985d36266ee4a0
Invigo <invigo.devel@gmail.com>
2019-04-18 21:31:14 +0300

    sieve solution

--- sieve/Sieve.fs
+++ sieve/Sieve.fs
@@ -1,7 +1,25 @@
 ﻿module Sieve
 
-let rec sieve primes = function
-    | head::tail -> sieve (head::primes) (tail |> List.filter (fun curr -> curr % head > 0)) 
-    | [] -> primes |> List.rev
+open System
+open System.Collections
 
-let primes limit = sieve [] [2..limit]
+let sieve limit = 
+    let primeSieve = Array.create limit true
+    let maxp = sqrt (float (limit)) |> int
+    primeSieve.[0] <- false
+    primeSieve.[1] <- false
+    primeSieve.[2] <- true
+    for p in 2..(maxp + 1) do
+        if primeSieve.[p] then
+            for pm in (p * 2)..p..(limit) do
+                primeSieve.[pm] <- false
+    primeSieve
+
+let primes limit = match limit with
+                   | 0 | 1 -> []
+                   | _ -> sieve limit
+                          |> Seq.mapi (fun i v -> v, i)
+                          |> Seq.filter fst
+                          |> Seq.map snd
+                          |> Seq.toList
+
