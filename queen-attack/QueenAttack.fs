527147aabf6ce56e39c9b8abd3c64f4158afddf3
Invigo <invigo.devel@gmail.com>
2019-05-02 16:00:58 +0300

    queen-attack solution

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,7 +1,14 @@
 ﻿module QueenAttack
 
 let create (position: int * int) = 
-    failwith "You need to implement this function."
+    let (x, y) = position
+    let onChessBoard z = z >= 0 && z < 8
+    onChessBoard x && onChessBoard y
 
 let canAttack (queen1: int * int) (queen2: int * int) = 
-    failwith "You need to implement this function."
+    let (x1, y1) = queen1
+    let (x2, y2) = queen2
+    let horizontalAttack = x1 = x2
+    let verticalAttack   = y1 = y2
+    let diagonalAttack   = (x2 - x1 |> abs) = (y2 - y1 |> abs)
+    horizontalAttack || verticalAttack || diagonalAttack
