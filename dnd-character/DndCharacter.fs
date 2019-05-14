87fa222b66255f68fe0509342468e86c9b0b1b2a
Invigo <invigo.devel@gmail.com>
2019-05-14 17:50:57 +0300

    dnd-character solution

---
+++ dnd-character/DndCharacter.fs
@@ -0,0 +1,27 @@
+﻿module DndCharacter
+
+
+let modifier x =
+    float (x - 10) / 2.0 |> System.Math.Floor |> int
+
+let ability() = 
+    List.init 4 (fun _ -> System.Random().Next(6) + 1)
+    |> List.skip 1 
+    |> List.sum
+
+type DndCharacter() =
+    let strength = ability()
+    let dexterity = ability()
+    let constitution = ability()
+    let intelligence = ability()
+    let wisdom = ability()
+    let charisma = ability()
+    
+    member __.Strength with get() = strength
+    member __.Dexterity with get() = dexterity
+    member __.Constitution with get() = constitution
+    member __.Intelligence with get() = intelligence
+    member __.Wisdom with get() = wisdom
+    member __.Charisma with get() = charisma
+    member __.Hitpoints with get() = modifier(__.Constitution) + 10
+
