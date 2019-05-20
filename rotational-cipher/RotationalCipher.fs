fffd2093991d53e89cd06f142d767b2bd38a7918
Invigo <invigo.devel@gmail.com>
2019-05-20 17:06:27 +0300

    rotational-cipher solution

---
+++ rotational-cipher/RotationalCipher.fs
@@ -0,0 +1,15 @@
+﻿module RotationalCipher
+
+open System
+
+let rot26 key ch =
+    match ch with  
+    | ch when (ch |> Char.IsUpper) -> (int ch + key - int 'A') % 26 + (int 'A') |> char
+    | ch when (ch |> Char.IsLower) -> (int ch + key - int 'a') % 26 + (int 'a') |> char
+    | ch when (ch |> Char.IsLetter |> not) -> ch
+    | _ -> failwith "Error"
+
+let rotate shiftKey text = 
+    text
+    |> Seq.map (fun x -> rot26 shiftKey x)
+    |> String.Concat
