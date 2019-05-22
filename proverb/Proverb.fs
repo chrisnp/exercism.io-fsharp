a11e9570c67d45c62c154c3cdec546fd76ac84de
Invigo <invigo.devel@gmail.com>
2019-05-22 10:30:50 +0300

    proverb solution

---
+++ proverb/Proverb.fs
@@ -0,0 +1,12 @@
+﻿module Proverb
+
+let recite (input: string list): string list = 
+    match input with
+    | []  -> []
+    | [x] -> [ sprintf "And all for the want of a %s." x ]
+    | _   -> seq { yield! input 
+                          |> List.pairwise 
+                          |> List.map(fun (x, y) -> 
+                                      sprintf "For want of a %s the %s was lost." <|| (x, y))
+                   yield  sprintf "And all for the want of a %s." input.Head }
+             |> Seq.toList
