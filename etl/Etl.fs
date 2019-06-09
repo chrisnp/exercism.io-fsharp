f69154a9e305a718ef305aca06310705e8b92065
Invigo <invigo.devel@gmail.com>
2019-06-09 10:15:09 +0300

    etl - 1st iteration

--- etl/Etl.fs
+++ etl/Etl.fs
@@ -1,3 +1,11 @@
 ﻿module Etl
 
-let transform (scoresWithLetters: Map<int, char list>): Map<char, int> = failwith "You need to implement this function."
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> = 
+
+    let flipPairs (score : int, letters : char list) = 
+        letters |> Seq.map (fun letter -> (char ((string letter).ToLowerInvariant()), score))
+
+    scoresWithLetters 
+    |> Map.toSeq
+    |> Seq.collect flipPairs
+    |> Map.ofSeq
