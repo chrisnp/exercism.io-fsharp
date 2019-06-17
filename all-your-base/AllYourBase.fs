fe333d7a28a752e78b297056cc1691f3eb88dd13
Invigo <invigo.devel@gmail.com>
2019-06-17 13:19:52 +0300

    all-your-base solution

--- all-your-base/AllYourBase.fs
+++ all-your-base/AllYourBase.fs
@@ -1,38 +1,37 @@
 ﻿module AllYourBase
-open FsUnit
 
 let inline (><) (x, y) = y, x 
 
 let rebase digits inputBase outputBase = 
     let errors =
-        [inputBase;outputBase] |> List.exists ((<=) 2)
-        || digits |> List.exists ((<) 0)
-        || digits |> List.exists ((>=) inputBase)
+        [inputBase;outputBase] |> List.exists (fun x -> x <= 1)
+        || digits |> List.exists (fun x -> x < 0)
+        || digits |> List.exists (fun x -> x >= inputBase)
 
     let degenerate =
-        digits |> List.isEmpty 
-        || digits |> List.forall((=) 0)
-       
+        digits |> List.forall(fun x -> x = 0)
 
-    let representationIn newBase num =
-        if num > 0 then 
-            num
-            |> List.unfold (fun (x: int) -> 
-                    if x = 0 then 
-                        None 
-                    else 
-                        Some ((><) <| System.Math.DivRem(x, newBase)))  
-            |> List.rev
-        else
-            List.empty
+    let numberIn (newBase : int) (digits : int list) = 
+        let len = digits.Length
+        digits 
+        |> List.mapi (fun p d -> d * int(double(newBase) ** double(len - 1 - p)))
+        |> List.sum
+
+    let digitsIn (newBase : int) (number : int) = 
+        number
+        |> List.unfold (fun x -> 
+                            if x = 0 then 
+                                None 
+                            else 
+                                Some((><) <| System.Math.DivRem(x, newBase)))
+        |> List.rev
 
     if   errors then 
         None
     elif degenerate then 
         Some[0]
     else
-        digits
-        |> Seq.zip [(List.length digits - 1) .. -1 ..0]
-        |> Seq.sumBy (fun (i, d) -> d * (int) ((float) inputBase ** (float) i))
-        |> representationIn outputBase
+        digits 
+        |> numberIn inputBase
+        |> digitsIn outputBase
         |> Some
