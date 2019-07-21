6c111c8c25f8b313fcf4c0b760e78e9a8b6d3828
Invigo <invigo.devel@gmail.com>
2019-07-21 18:15:06 +0300

    word-count solution

---
+++ word-count/WordCount.fs
@@ -0,0 +1,21 @@
+﻿module WordCount
+
+open System 
+
+let countWords (phrase: string) = 
+    let allowed (ch: char) =
+        ch |> Char.IsDigit 
+        || ch |> Char.IsLetter 
+        || Array.contains ch [|' ';'\'';','|]
+    let sanitized = 
+        phrase.ToLowerInvariant()
+        |> Seq.filter allowed
+        |> String.Concat
+    let tokens = 
+        sanitized.Split([|' '; ','|])
+        |> Seq.filter (String.IsNullOrEmpty >> not)
+        |> Seq.map (fun x -> x.Trim('''))
+    tokens
+    |> Seq.groupBy id
+    |> Map.ofSeq
+    |> Map.map (fun i v -> Seq.length v)
