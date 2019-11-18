fa8fa9ee821f403ef3685b0767d1de6330dc3baa
Invigo <invigo.devel@gmail.com>
2019-11-18 02:35:29 +0200

    series solution - 1st iteration

---
+++ series/Series.fs
@@ -0,0 +1,12 @@
+﻿module Series
+
+let slices (str : string) (length : int) = 
+    if length > str.Length || length <= 0
+    then 
+        None
+    else
+        str
+        |> Seq.windowed length
+        |> (Seq.map <| System.String)
+        |> Seq.toList
+        |> Some        
