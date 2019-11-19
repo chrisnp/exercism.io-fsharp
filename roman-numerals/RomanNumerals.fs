7eda75695bac356f94186f091452b27e70a82c7f
Invigo <invigo.devel@gmail.com>
2019-11-19 02:37:51 +0200

    roman-numerals solution refactor

--- roman-numerals/RomanNumerals.fs
+++ roman-numerals/RomanNumerals.fs
@@ -1,31 +1,23 @@
 ﻿module RomanNumerals
 
-let rec roman arabicNumeral = 
-    if arabicNumeral >= 1000 then 
-        "M" + roman(arabicNumeral - 1000)
-    elif arabicNumeral >= 900 then 
-        "CM" + roman(arabicNumeral - 900)
-    elif arabicNumeral >= 500 then 
-        "D" + roman(arabicNumeral - 500)
-    elif arabicNumeral >= 400 then 
-        "CD" + roman(arabicNumeral - 400)
-    elif arabicNumeral >= 100 then 
-        "C" + roman(arabicNumeral - 100)
-    elif arabicNumeral >= 90 then 
-        "XC" + roman(arabicNumeral - 90)
-    elif arabicNumeral >= 50 then 
-        "L" + roman(arabicNumeral - 50)
-    elif arabicNumeral >= 40 then 
-        "XL" + roman(arabicNumeral - 40)
-    elif arabicNumeral >= 10 then 
-        "X" + roman(arabicNumeral - 10)
-    elif arabicNumeral >= 9 then 
-        "IX" + roman(arabicNumeral - 9)
-    elif arabicNumeral >= 5 then 
-        "V" + roman(arabicNumeral - 5)
-    elif arabicNumeral >= 4 then 
-        "IV" + roman(arabicNumeral - 4)
-    elif arabicNumeral >= 1 then 
-        "I" + roman(arabicNumeral - 1)
-    else ""
-    
+let romanNumerals = [ ("M", 1000);
+                      ("CM", 900); 
+                      ("D", 500); 
+                      ("CD", 400); 
+                      ("C", 100);
+                      ("XC", 90); 
+                      ("L", 50); 
+                      ("XL", 40); 
+                      ("X", 10);
+                      ("IX", 9); 
+                      ("V", 5); 
+                      ("IV", 4); 
+                      ("I", 1); ]
+
+let roman arabicNumeral = 
+    let rec roman (acc, num) (symbol, value) = 
+        if (num < value) then 
+            (acc, num) 
+        else 
+            roman (acc + symbol, num - value) (symbol, value)
+    List.fold roman ("", arabicNumeral) romanNumerals |> fst
