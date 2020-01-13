ed2a76e4fb2b3f81fc2c5866571ff224fa42e373
Invigo <invigo.devel@gmail.com>
2020-01-13 03:50:39 +0200

    Refactor ArmstrongNumbers.fs

--- armstrong-numbers/ArmstrongNumbers.fs
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -1,10 +1,9 @@
 ﻿module ArmstrongNumbers
 
 let isArmstrongNumber (number: int): bool = 
-
-    let numberOfDigits = string <| number |> String.length
-
-    [for char in string <| number -> int <| (char |> string)]
-    |> List.sumBy (fun x -> int <| float x ** float numberOfDigits)
+    let digits = [for ch in string number -> int (ch.ToString())]
+    let length = string <| number |> String.length
+    digits 
+    |> List.sumBy (fun x -> int (float x ** float length))
     |> (=) number
 
