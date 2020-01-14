5b914d0da9fb9d9ef86a6476a4f827d6cbf6376e
Invigo <invigo.devel@gmail.com>
2020-01-14 23:00:07 +0200

    Refactor Hamming.fs

--- hamming/Hamming.fs
+++ hamming/Hamming.fs
@@ -1,10 +1,11 @@
 ﻿module Hamming
 
 let distance (strand1: string) (strand2: string): int option = 
-    if (strand1.Length <> strand2.Length) then
-        None
-    else 
+    if (strand1.Length = strand2.Length) then
         Seq.zip strand1 strand2
         |> Seq.filter (fun (x, y) -> x <> y)
         |> Seq.length
         |> Some
+    else 
+        None
+        
