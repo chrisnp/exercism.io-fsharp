b94f4f1e4bdc3cfbca2b84391ded3eacd1fc643c
Invigo <invigo.devel@gmail.com>
2020-01-14 22:53:30 +0200

    Reformat SimpleCipher.fs

--- simple-cipher/SimpleCipher.fs
+++ simple-cipher/SimpleCipher.fs
@@ -4,12 +4,19 @@ open System
 
 let private charToIndex c = int c - int 'a'
 let private indexToChar i = i + int 'a' |> char
-let private mod26 (x: int) = if x < 0 then x + 26 else x % 26
+let private mod26 (x: int) = if x < 0 
+                             then 
+                                x + 26 
+                             else 
+                                x % 26
 
 let private xShift (input:string) (key:string) (sign:int) = 
     input 
     |> Seq.zip ( seq{ while true do for i in key -> i } )
-    |> Seq.map (fun (k, c) -> ((c |> charToIndex) + sign * (k |> charToIndex)) |> mod26 |> indexToChar)
+    |> Seq.map (fun (k, c) -> 
+                    ((c |> charToIndex) + sign * (k |> charToIndex)) 
+                     |> mod26 
+                     |> indexToChar)
     |> String.Concat
 
 type SimpleCipher(key: string) =
@@ -20,5 +27,6 @@ type SimpleCipher(key: string) =
     
     member __.Decode(ciphertext) = xShift ciphertext key (-1)
     
-    new() = SimpleCipher ([for i in 1 .. 100 do yield char (int 'a' + Random().Next(26))]
+    new() = SimpleCipher ([for i in 1 .. 100 do 
+                                yield char (int 'a' + Random().Next(26))]
                           |> String.Concat)
