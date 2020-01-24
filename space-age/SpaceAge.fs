efd3ed036b0bddb27983f7788b69b25b1e965f64
Invigo <invigo.devel@gmail.com>
2020-01-24 11:36:10 +0200

    Refactor solution

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -12,19 +12,20 @@ type Planet =
     | Neptune
     | Mercury
 
-let orbitalPeriodOf = 
-    function
-    | Mercury -> 0.2408467
-    | Venus   -> 0.61519726
-    | Earth   -> 1.0
-    | Mars    -> 1.8808158 
-    | Jupiter -> 11.862615
-    | Saturn  -> 29.447498
-    | Uranus  -> 84.016846
-    | Neptune -> 164.79132
     
 let age (planet: Planet) (seconds: int64): float = 
-    let round (n : float) = Math.Round (n, 2)
-    let earthYear = 31557600.0
-    float seconds / (earthYear  * orbitalPeriodOf planet)
-    |> round
+    let orbitalPeriodOf = 
+        function | Mercury -> 0.2408467
+                 | Venus   -> 0.61519726
+                 | Earth   -> 1.0
+                 | Mars    -> 1.8808158 
+                 | Jupiter -> 11.862615
+                 | Saturn  -> 29.447498
+                 | Uranus  -> 84.016846
+                 | Neptune -> 164.79132
+    let round (n : float) = 
+        Math.Round (n, 2)
+    let planetYear = 
+        31557600.0 * orbitalPeriodOf planet
+    round <| float seconds / planetYear
+    
