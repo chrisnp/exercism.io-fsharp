d9377e6850b8d8bfb0aaf697ddfe19073fbf09c1
Invigo <invigo.devel@gmail.com>
2020-01-24 11:18:51 +0200

    Refactor Leap.fs

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,3 +1,11 @@
 ﻿module Leap
 
-let leapYear (year: int): bool = year % 4 = 0 && (year % 400 = 0 || year % 100 <> 0)
+let leapYear (year: int): bool = 
+    let by400 = year % 400
+    let by100 = year % 100
+    let by004 = year % 4
+    match (by400, by100, by004) with
+    | (0, 0, 0) -> true
+    | (_, 0, 0) -> false
+    | (_, _, 0) -> true
+    | (_, _, _) -> false
