0f109c4f1be235bfbf5ff01d0bad7d9f2485a235
Invigo <invigo.devel@gmail.com>
2020-02-14 21:14:09 +0200

    Refactor GradeSchool.fs

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -2,21 +2,30 @@
 
 type School = Map<int, string list>
 
+
 let empty : School = Map.empty
 
-let grade (num: int) (school: School): string list = 
-    match school |> Map.tryFind num with
+
+let grade (number : int) 
+          (school : School) : string list = 
+    match school.TryFind number with
     | Some students -> students
     | None -> []
 
-let add (student: string) (inGrade: int) (school: School): School = 
-    let current = school |> grade inGrade
-    school 
-    |> Map.add inGrade ((student :: current) |> List.sort)
 
-let roster (school: School): string list = 
+let add (student : string) 
+        (inGrade : int) 
+        (school : School) : School = 
+    let current = school |> grade inGrade
     school
-    |> Map.toList
-    |> List.sort
-    |> List.collect (snd >> List.sort)
+    |> Map.add inGrade ((student :: current) 
+                        |> List.sort)
+
+
+let roster (school : School) : string list = 
+    school
+    |> Map.toSeq
+    |> Seq.sort
+    |> Seq.collect (snd >> List.sort)
+    |> Seq.toList
 
