6035b317ff1205667a7269a90d0023543637e765
Invigo <invigo.devel@gmail.com>
2020-02-17 23:41:57 +0200

    Update Darts.fs

--- darts/Darts.fs
+++ darts/Darts.fs
@@ -3,7 +3,7 @@
 let score (x: double) (y: double): int = 
     let r = x**2. + y**2. |> sqrt
     match r with 
-        | r when r <= 1.  -> 10
-        | r when r <= 5.  -> 5
-        | r when r <= 10. -> 1
-        | _ -> 0
+    | r when r <= 1.  -> 10
+    | r when r <= 5.  -> 5
+    | r when r <= 10. -> 1
+    | _ -> 0
