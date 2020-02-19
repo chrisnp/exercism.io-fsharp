93f47359289f7e6c3dac55aa74ca7e2eaac4cb51
Zero Delta <zerodeltahedge@gmail.com>
2020-02-20 01:17:39 +0200

    Update ProteinTranslation.fs

--- protein-translation/ProteinTranslation.fs
+++ protein-translation/ProteinTranslation.fs
@@ -3,15 +3,24 @@
 open System
 
 let translateCodon = function
-  | "AUG" -> "Methionine"
-  | "UUU" | "UUC" -> "Phenylalanine"
-  | "UUA" | "UUG" -> "Leucine"
-  | "UCU" | "UCC" | "UCA" | "UCG" -> "Serine"
-  | "UAU" | "UAC" -> "Tyrosine"
-  | "UGU" | "UGC" -> "Cysteine"
-  | "UGG" -> "Tryptophan"
-  | "UAA" | "UAG" | "UGA" -> "STOP"
-  | other -> failwithf "Not valid codon: %s" other
+  | "AUG" -> 
+      "Methionine"
+  | "UUU" | "UUC" -> 
+      "Phenylalanine"
+  | "UUA" | "UUG" -> 
+      "Leucine"
+  | "UCU" | "UCC" | "UCA" | "UCG" -> 
+      "Serine"
+  | "UAU" | "UAC" -> 
+      "Tyrosine"
+  | "UGU" | "UGC" -> 
+      "Cysteine"
+  | "UGG" -> 
+      "Tryptophan"
+  | "UAA" | "UAG" | "UGA" -> 
+      "STOP"
+  | other -> 
+      failwithf "Not valid codon: %s" other
 
 let proteins rna = 
     let stopped cod = cod = "STOP"
