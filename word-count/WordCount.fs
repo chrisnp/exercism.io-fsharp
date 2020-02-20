5339ce3f1077411eebf1cbbf18512caac80092de
Invigo <invigo.devel@gmail.com>
2020-02-20 23:42:25 +0200

    Update WordCount.fs

--- word-count/WordCount.fs
+++ word-count/WordCount.fs
@@ -4,22 +4,18 @@ open System
 
 
 let countWords (phrase: string) = 
-    
     let allowed (ch: char) =
         ch |> Char.IsDigit 
         || ch |> Char.IsLetter 
         || Array.contains ch [|' ';'\'';','|]
-    
     let sanitized = 
         phrase.ToLowerInvariant()
         |> Seq.filter allowed
         |> String.Concat
-    
     let tokens = 
         sanitized.Split([|' '; ','|])
         |> Seq.filter (String.IsNullOrEmpty >> not)
         |> Seq.map (fun x -> x.Trim('''))
-    
     tokens
     |> Seq.groupBy id
     |> Map.ofSeq
