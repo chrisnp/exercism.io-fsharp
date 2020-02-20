e7279e007193d7073abef182adc93eca87c4e84c
Invigo <invigo.devel@gmail.com>
2020-02-20 23:36:27 +0200

    Update Anagram.fs

--- anagram/Anagram.fs
+++ anagram/Anagram.fs
@@ -4,7 +4,10 @@ open System
 
 let findAnagrams (sources: string list) (target: string) = 
     
-    let normal (x: string) = x.ToLower() |> Seq.sortBy id |> Seq.toList
+    let normal (x: string) = 
+        x.ToLower() 
+        |> Seq.sortBy id 
+        |> Seq.toList
 
     List.filter (fun x -> normal x = normal target) sources
     |> List.filter (fun x -> x.ToLower() <> target.ToLower())
