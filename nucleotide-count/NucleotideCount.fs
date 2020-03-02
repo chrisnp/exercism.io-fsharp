258b0352e72fd5e54c6027c47f4f5d2b54b5e888
Invigo <invigo.devel@gmail.com>
2020-03-02 15:48:27 +0200

    Update NucleotideCount.fs

--- nucleotide-count/NucleotideCount.fs
+++ nucleotide-count/NucleotideCount.fs
@@ -1,11 +1,16 @@
 ﻿module NucleotideCount
 
+
 let nucleotides = ['A';'C';'G';'T']
 
 let nucleotideCounts (strand: string): Option<Map<char, int>> =  
-    match strand.ToCharArray() |> Array.forall (fun x -> Seq.contains x nucleotides) with
+    match strand.ToCharArray() 
+          |> Array.forall (fun x -> 
+                           Seq.contains x nucleotides) with
     | true -> nucleotides
-              |> Seq.map (fun x -> x, strand |> Seq.filter ((=) x) |> Seq.length)
+              |> Seq.map (fun x -> x, strand 
+                                      |> Seq.filter ((=) x) 
+                                      |> Seq.length)
               |> Map.ofSeq
               |> Some
     | false -> None
