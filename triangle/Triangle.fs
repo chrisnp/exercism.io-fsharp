0de1430c2565323775d97516c36fb6129090c1d0
Invigo <invigo.devel@gmail.com>
2020-03-04 01:47:06 +0200

    Sanitize .testRun

--- triangle/Triangle.fs
+++ triangle/Triangle.fs
@@ -1,21 +1,26 @@
 ﻿module Triangle
 
 let private isATriangle triangle =
-    let nonZero = List.sum triangle <> 0.0
+    let nonZero = 
+        List.sum triangle <> 0.0
     let inequality =
         let [a; b; c] = triangle
-        a + b >= c && a + c >= b && b + c >= a
+        a + b >= c && 
+        a + c >= b && 
+        b + c >= a
     inequality && nonZero
 
-let private distinctSides triangle = triangle 
-                                     |> List.distinct 
-                                     |> List.length
+let private distinctSides triangle = 
+    triangle |> List.distinct |> List.length
 
-let equilateral triangle = (triangle |> isATriangle) && 
-                           (triangle |> distinctSides = 1)
+let equilateral triangle = 
+    (triangle |> isATriangle) && 
+    (triangle |> distinctSides = 1)
 
-let isosceles triangle = (triangle |> isATriangle) && 
-                         (triangle |> distinctSides <= 2)
+let isosceles triangle = 
+    (triangle |> isATriangle) && 
+    (triangle |> distinctSides <= 2)
 
-let scalene triangle = (triangle |> isATriangle) && 
-                       (triangle |> distinctSides = 3)
+let scalene triangle = 
+    (triangle |> isATriangle) && 
+    (triangle |> distinctSides = 3)
