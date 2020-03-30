fa43cbbf917ff1fc5a580ad2faa5486d2b71e0d8
Invigo <invigo.devel@gmail.com>
2020-03-30 23:07:56 +0300

    Update ErrorHandling.fs

--- error-handling/ErrorHandling.fs
+++ error-handling/ErrorHandling.fs
@@ -5,18 +5,25 @@ let handleErrorByThrowingException() =
 
 let handleErrorByReturningOption (input:string) = 
     match System.Char.IsDigit(char input) with 
-    | true -> Some (int input)
-    | _ -> None
+    | true -> 
+        Some (int input)
+    | _ -> 
+        None
 
 let handleErrorByReturningResult (input:string) = 
     match System.Char.IsDigit(char input) with
-    | true -> Ok (int input)
-    | _ -> Error "Could not convert input to integer"
+    | true -> 
+        Ok (int input)
+    | _ -> 
+        Error "Could not convert input to integer"
 
 let bind switchFunction twoTrackInput = 
     match twoTrackInput with
-    | Ok x    -> switchFunction x
-    | Error e -> Error e
+    | Ok x    -> 
+        switchFunction x
+    | Error e -> 
+        Error e
 
-let cleanupDisposablesWhenThrowingException  resource = 
-    using (resource) ( fun _ -> raise(System.Exception()))
+let cleanupDisposablesWhenThrowingException resource = 
+    using (resource) ( fun _ -> 
+                            raise(System.Exception()))
