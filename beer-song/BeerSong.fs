dc0bc55bdc1dd5d350538ed4a5fdca380b088674
Invigo <invigo.devel@gmail.com>
2020-05-01 14:19:48 +0300

    Update BeerSong.fs

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -12,8 +12,12 @@ let private stanza num =
         ["2 bottles of beer on the wall, 2 bottles of beer."; 
          "Take one down and pass it around, 1 bottle of beer on the wall."]
     | _ -> 
-        [sprintf "%d bottles of beer on the wall, %d bottles of beer." num num;
-         sprintf "Take one down and pass it around, %d bottles of beer on the wall." (num-1)]
+        [sprintf 
+            "%d bottles of beer on the wall, %d bottles of beer." 
+            num num;
+         sprintf 
+            "Take one down and pass it around, %d bottles of beer on the wall." 
+            (num - 1)]
 
 let recite (startBottles: int) (takeDown: int) = 
     [startBottles .. -1 .. startBottles - takeDown + 1] 
