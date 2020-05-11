b7c810bb967263ac58913b6bf19a293fa36b4eee
Invigo <invigo.devel@gmail.com>
2020-05-11 16:25:50 +0300

    Refactor Darts

--- darts/Darts.fs
+++ darts/Darts.fs
@@ -1,9 +1,9 @@
 ﻿module Darts
 
 let score (x: double) (y: double): int = 
-    match sqrt <| x**2. + y**2.
+    match sqrt <| (+) (x**2.) (y**2.)
         with 
-        | r when r <= 1.  -> 10
-        | r when r <= 5.  -> 5
+        | r when r <= 1. -> 10
+        | r when r <= 5. ->  5
         | r when r <= 10. -> 1
         | _ -> 0
