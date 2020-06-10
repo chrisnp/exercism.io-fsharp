f788ccc6b889fb047bcabc41d8bf8b33be2f4f1f
Invigo <invigo.devel@gmail.com>
2020-06-10 16:36:20 +0300

    Update QueenAttack.fs

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -8,9 +8,15 @@ let create (position: int * int) =
 let canAttack (queen1: int * int) (queen2: int * int) = 
     let (x1, y1) = queen1
     let (x2, y2) = queen2
-    match (x1 - x2 |> abs, y1 - y2 |> abs) with
-    | (0, 0)                -> failwith "Error: Both queens on same square"
-    | (0, _)                -> true // horizontal attack
-    | (_, 0)                -> true // vertical attack
-    | (x, y) when x / y = 1 -> true // diagonal attack
-    | (_, _)                -> false
+    match (x1 - x2 |> abs, y1 - y2 |> abs) 
+        with
+        | (0, 0) -> 
+            failwith "Error: Both queens on same square"
+        | (0, _) -> 
+            true // horizontal attack
+        | (_, 0) -> 
+            true // vertical attack
+        | (x, y) when x / y = 1 -> 
+            true // diagonal attack
+        | (_, _) -> 
+            false
