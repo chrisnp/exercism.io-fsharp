4b63658713906a0250b522e82661952a6917cbb0
Invigo <invigo.devel@gmail.com>
2020-07-07 23:06:05 +0300

    Update RobotName.fs

--- robot-name/RobotName.fs
+++ robot-name/RobotName.fs
@@ -3,15 +3,20 @@
 open System
 
 let generateName() = 
-    let alpha = ['A'..'Z']
-    let random = Random()
+    let alpha = 
+        ['A'..'Z']
+    let random = 
+        Random()
     string (alpha.[random.Next(26)]) + 
     string (alpha.[random.Next(26)]) + 
     (string (random.Next(999)))
 
 type Robot = { name : string }
 
-let mkRobot() = { name = generateName() }
-let name robot = robot.name
-let reset robot = { robot with name = generateName() } 
+let mkRobot() = 
+    { name = generateName() }
+let name robot = 
+    robot.name
+let reset robot = 
+    { robot with name = generateName() } 
 
