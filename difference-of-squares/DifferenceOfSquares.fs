37502f60ae30d315ca6707d554939e5d54bcbcf4
Invigo <invigo.devel@gmail.com>
2020-07-20 23:21:42 +0300

    Update DifferenceOfSquares.fs

--- difference-of-squares/DifferenceOfSquares.fs
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -1,9 +1,13 @@
 ﻿module DifferenceOfSquares
 
-let square (number : int) : int = number * number
+let square (number : int) : int = 
+        number * number
 
-let squareOfSum (number : int) : int = [1 .. number] |> List.sum |> square 
+let squareOfSum (number : int) : int = 
+        [1 .. number] |> List.sum |> square 
 
-let sumOfSquares (number : int) : int = [1 .. number] |> List.sumBy (square)  
+let sumOfSquares (number : int) : int = 
+        [1 .. number] |> List.sumBy (square)  
 
-let differenceOfSquares (number : int) : int = squareOfSum(number) - sumOfSquares(number)
+let differenceOfSquares (number : int) : int = 
+        squareOfSum(number) - sumOfSquares(number)
