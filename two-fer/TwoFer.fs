e37b939e9510aa8855a196ab3fc028b25c5face2
Invigo <invigo.devel@gmail.com>
2020-07-27 17:51:21 +0300

    Update TwoFer.fs

--- two-fer/TwoFer.fs
+++ two-fer/TwoFer.fs
@@ -1,6 +1,9 @@
 ﻿module TwoFer
 
 let twoFer (input: string option): string = 
-    match input with
-    | Some name -> sprintf "One for %s, one for me." name
-    | None      -> "One for you, one for me."
+    match input 
+        with
+        | Some name -> 
+            sprintf "One for %s, one for me." name
+        | None -> 
+            "One for you, one for me."
