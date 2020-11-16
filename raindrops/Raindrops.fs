03ec98b56a60e70b8103d61859166008da58f9b1
Invigo <invigo.devel@gmail.com>
2020-11-16 06:40:31 +0200

    Update Raindrops.fs

--- raindrops/Raindrops.fs
+++ raindrops/Raindrops.fs
@@ -1,8 +1,17 @@
 ﻿module Raindrops
 
 let convert (number: int): string =     
-    let rainFactors = [(3, "Pling"); (5, "Plang"); (7, "Plong")]
-    let drops = [for (n, s) in rainFactors do if number % n = 0 then yield s]  
+    let rainFactors = 
+        [(3, "Pling"); 
+         (5, "Plang"); 
+         (7, "Plong")]
+    let drops = 
+        [for (n, s) in rainFactors do 
+            if number % n = 0 
+            then yield s]  
     match drops with 
-    | []   -> string number
-    | drop -> drop |> String.concat ""
+    | []   -> 
+        string number
+    | drop -> 
+        drop 
+        |> String.concat ""
