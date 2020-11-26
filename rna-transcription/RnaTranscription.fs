0ae98c0d35886570f31387d371552c225722566f
Invigo <invigo.devel@gmail.com>
2020-11-26 21:35:25 +0200

    Update RnaTranscription.fs

--- rna-transcription/RnaTranscription.fs
+++ rna-transcription/RnaTranscription.fs
@@ -1,11 +1,11 @@
 module RnaTranscription
+
+let ERR = "Invalid nucleotide"
 let toRna (dna: string): string = 
     dna
-    |> String.map (fun x ->  
-                    match x with
-                       | 'G' -> 'C'
-                       | 'C' -> 'G'
-                       | 'T' -> 'A'
-                       | 'A' -> 'U'
-                       | _ -> 
-                            failwith "Invalid nucleotide")
+    |> String.map (fun x -> match x with
+                            | 'G' -> 'C'
+                            | 'C' -> 'G'
+                            | 'T' -> 'A'
+                            | 'A' -> 'U'
+                            | _ -> failwith ERR)
