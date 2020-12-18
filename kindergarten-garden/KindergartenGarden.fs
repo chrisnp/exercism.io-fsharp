bbe3fc604207a9330fdd90d1d776edbb78e65d06
Invigo <invigo.devel@gmail.com>
2020-12-18 22:51:32 +0200

    Reformat KindergartenGarden.fs

--- kindergarten-garden/KindergartenGarden.fs
+++ kindergarten-garden/KindergartenGarden.fs
@@ -1,12 +1,20 @@
 ﻿module KindergartenGarden
 
-type Plant = | Grass | Radishes | Clover | Violets | Unknown
+type Plant = | Grass 
+             | Radishes 
+             | Clover 
+             | Violets 
+             | Unknown
 
-type Garden = { Row0 : Plant list; Row1 : Plant list }
+type Garden = { Row0 : Plant list; 
+                Row1 : Plant list }
 
 let private students: string list = 
-        (Seq.sort >> Seq.toList) <| ["Alice";"Ginny";"Charlie";"David";"Joseph";"Fred"; 
-                                     "Bob";"Harriet";"Ileana";"Eve";"Kincaid";"Larry"]
+        (Seq.sort >> Seq.toList) 
+        <| ["Alice";"Ginny";"Charlie";
+            "David";"Joseph";"Fred"; 
+            "Bob";"Harriet";"Ileana";
+            "Eve";"Kincaid";"Larry"]
 
 let private decodePlant (code: char): Plant =
         match code with
@@ -18,11 +26,16 @@ let private decodePlant (code: char): Plant =
 
 let private garden (diagram: string): Garden = 
         let rows  = diagram.Split('\n')
-                    |> Seq.map (Seq.map decodePlant >> Seq.toList)
+                    |> Seq.map (Seq.map decodePlant 
+                                >> Seq.toList)
                     |> Seq.toList
-        {Row0 = rows.[0]; Row1 = rows.[1]}      
+        {Row0 = rows.[0]; 
+         Row1 = rows.[1]}      
 
 let plants diagram student = 
-        let i = students |> List.findIndex ((=) student) |> (*) 2
-        [(garden diagram).Row0.[i]; (garden diagram).Row0.[i + 1]
-         (garden diagram).Row1.[i]; (garden diagram).Row1.[i + 1]]
+        let i = students 
+                |> List.findIndex ((=) student) |> (*) 2
+        [(garden diagram).Row0.[i]; 
+         (garden diagram).Row0.[i + 1]
+         (garden diagram).Row1.[i]; 
+         (garden diagram).Row1.[i + 1]]
