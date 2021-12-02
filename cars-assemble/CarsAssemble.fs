89de699766db0fea63efb6c3d6d33c18a41c28a2
Invigo <invigo.devel@gmail.com>
2021-12-02 18:18:47 +0200

    1st passing iteration - CarsAssemble.fs

--- cars-assemble/CarsAssemble.fs
+++ cars-assemble/CarsAssemble.fs
@@ -1,12 +1,21 @@
 module CarsAssemble
 
-let private carsPerHour = 221
+let private carsPerHour = 221.
 
 let successRate (speed: int): float =
-    failwith "Please implement the 'successRate' function"
+        match speed with
+        | 0 -> 0.0
+        | 1 | 2 | 3 | 4 -> 1.0
+        | 5 | 6 | 7 | 8 -> 0.9
+        | 9  -> 0.8
+        | 10 -> 0.77
+        | _ -> failwith "Invalid Speed"
 
 let productionRatePerHour (speed: int): float =
-    failwith "Please implement the 'productionRatePerHour' function"
+    speed
+    |> successRate
+    |> (*) (speed |> float)
+    |> (*) carsPerHour
 
 let workingItemsPerMinute (speed: int): int =
-    failwith "Please implement the 'workingItemsPerMinute' function"
+    productionRatePerHour speed / 60.0 |> int
