37a7e09b85263238e1a233c7b02d7b3dca47daa0
Invigo <invigo.devel@gmail.com>
2021-12-02 17:48:31 +0200

    Create CarsAssemble.fs

---
+++ cars-assemble/CarsAssemble.fs
@@ -0,0 +1,12 @@
+module CarsAssemble
+
+let private carsPerHour = 221
+
+let successRate (speed: int): float =
+    failwith "Please implement the 'successRate' function"
+
+let productionRatePerHour (speed: int): float =
+    failwith "Please implement the 'productionRatePerHour' function"
+
+let workingItemsPerMinute (speed: int): int =
+    failwith "Please implement the 'workingItemsPerMinute' function"
