a26a260e6e7f02b2171c541edc239e187eb8c726
Invigo <invigo.devel@gmail.com>
2021-12-17 17:07:19 +0200

    1st passing iteration - interest-is-interesting

--- interest-is-interesting/InterestIsInteresting.fs
+++ interest-is-interesting/InterestIsInteresting.fs
@@ -1,21 +1,20 @@
 module InterestIsInteresting
 
 let interestRate (balance: decimal): single =
+   balance |>
    function 
    | b when b <  0m    -> 3.213f
    | b when b <  1000m -> 0.5f
    | b when b <  5000m -> 1.621f
    | b when b >= 5000m -> 2.475f
    | _ -> 0f
-   <| balance
 
 let interest (balance: decimal): decimal =
    let rate = (/) (balance |> interestRate |> decimal) 100m
    balance |> (*) rate
 
 let annualBalanceUpdate(balance: decimal): decimal =
-   balance 
-   |> (+) (balance |> interest)
+   balance |> (+) (balance |> interest)
 
 let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
    let donationRate = (/) (taxFreePercentage |> decimal) 100m
