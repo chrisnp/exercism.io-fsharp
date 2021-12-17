ea7b26a59cec7904df6bcce2254b5fda3a2133ee
Invigo <invigo.devel@gmail.com>
2021-12-17 17:03:27 +0200

    Update InterestIsInteresting.fs

--- interest-is-interesting/InterestIsInteresting.fs
+++ interest-is-interesting/InterestIsInteresting.fs
@@ -1,23 +1,27 @@
 module InterestIsInteresting
 
 let interestRate (balance: decimal): single =
-   balance |>
    function 
    | b when b <  0m    -> 3.213f
    | b when b <  1000m -> 0.5f
-   | b when b <  1000m -> 1.621f
+   | b when b <  5000m -> 1.621f
    | b when b >= 5000m -> 2.475f
    | _ -> 0f
+   <| balance
 
 let interest (balance: decimal): decimal =
-   (*) balance ((balance |> interestRate) |> decimal) 
+   let rate = (/) (balance |> interestRate |> decimal) 100m
+   balance |> (*) rate
 
 let annualBalanceUpdate(balance: decimal): decimal =
-   (+) balance (balance |> interest)
+   balance 
+   |> (+) (balance |> interest)
 
 let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
+   let donationRate = (taxFreePercentage |> decimal) |> (/) 100m
    if balance > 0m then
-      let donation = (*) 2m balance ((0.01f * taxFreePercentage) |> decimal)
-      donation |> floor |> int
-   else 
-      0
+      balance 
+      |> (*) 2m 
+      |> (*) donationRate 
+      |> int
+   else 0
