ff9510e67e1ff6536158ec7a91491535fe838ff3
Invigo <invigo.devel@gmail.com>
2021-12-22 17:23:52 +0200

    Update LogLevels.fs

--- log-levels/LogLevels.fs
+++ log-levels/LogLevels.fs
@@ -1,7 +1,10 @@
 module LogLevels
 
-let message (logLine: string): string = failwith "Please implement the 'message' function"
+let message (logLine: string): string = 
+    logLine.Remove(0, logLine.IndexOf(":") + 1).Trim()
 
-let logLevel(logLine: string): string = failwith "Please implement the 'logLevel' function"
+let logLevel(logLine: string): string = 
+    logLine.Remove(0, 1).Remove(logLine.IndexOf("]") - 1).ToLower()
 
-let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
+let reformat(logLine: string): string = 
+    $"""{logLine |> message} ({logLine |> logLevel})"""
