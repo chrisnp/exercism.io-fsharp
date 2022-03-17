b6fc5249efa7907ea4e17311c3212e60042e0acf
Invigo <invigo.devel@gmail.com>
2022-03-18 00:12:02 +0200

    Update PrimeFactors.fs

--- prime-factors/PrimeFactors.fs
+++ prime-factors/PrimeFactors.fs
@@ -1,3 +1,15 @@
-module PrimeFactors
+﻿module PrimeFactors
 
-let factors number = failwith "You need to implement this function."
+let factors number = 
+    let rec sieve num f fs =
+        if num = 1L then 
+            fs
+        elif num < f * f then
+            num :: fs
+        elif num % f = 0L then
+            sieve (num / f) f  (f :: fs)
+        else
+            sieve num (f + 1L) fs
+    sieve number 2L []
+    |> List.map (fun n -> n |> int)
+    |> List.rev
