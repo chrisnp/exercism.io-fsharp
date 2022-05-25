7448ffb601ee005b8abe78558f7c284a584786b0
Invigo <invigo.devel@gmail.com>
2022-05-25 20:46:26 +0300

    scrabble-score\ScrabbleScore.fs, 1st passing iteration

--- scrabble-score/ScrabbleScore.fs
+++ scrabble-score/ScrabbleScore.fs
@@ -1,3 +1,18 @@
 module ScrabbleScore
 
-let score word = failwith "You need to implement this function."
+open System
+
+let private letterValue = function
+    | 'Q' | 'Z'                   -> 10
+    | 'J' | 'X'                   -> 8
+    | 'K'                         -> 5
+    | 'F' | 'H' | 'V' | 'W' | 'Y' -> 4
+    | 'B' | 'C' | 'M' | 'P'       -> 3
+    | 'D' | 'G'                   -> 2
+    | 'A' | 'E' | 'I' | 'O' | 'U'
+    | 'L' | 'N' | 'R' | 'S' | 'T' -> 1
+    | _                           -> 0
+
+let score (word: string) = word 
+    |> Seq.sumBy (Char.ToUpperInvariant 
+                  >> letterValue)
