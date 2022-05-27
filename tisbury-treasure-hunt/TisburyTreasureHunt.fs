b8de05c498dea6dc426364a39fc25e8956b69f2e
Invigo <invigo.devel@gmail.com>
2022-05-27 23:56:20 +0300

    Update TisburyTreasureHunt.fs

--- tisbury-treasure-hunt/TisburyTreasureHunt.fs
+++ tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -1,13 +1,22 @@
 module TisburyTreasureHunt
 
 let getCoordinate (line: string * string): string =
-    failwith "Please implement the 'getCoordinate' function"
+    let (_, coordinate) = line
+    coordinate
 
 let convertCoordinate (coordinate: string): int * char = 
-    failwith "Please implement the 'convertCoordinate' function"
+    match coordinate |> Seq.toList with
+    | [a;b] -> (a |> string |> int, b)
+    | _ -> failwith "Invalid coordinate"
 
 let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
-    failwith "Please implement the 'compareRecords' function"
+    let (_, ruisCoordinate, _) = ruisData
+    ruisCoordinate = (azarasData |> getCoordinate |> convertCoordinate)
 
 let createRecord (azarasData: string * string) (ruisData: string * (int * char) * string) : (string * string * string * string) =
-    failwith "Please implement the 'createRecord' function"
+    if compareRecords azarasData ruisData then
+        let (azarasName, coordinate) = azarasData
+        let (ruisName, _, color) = ruisData
+        (coordinate, ruisName, color, azarasName)
+    else
+        ("", "", "", "")
