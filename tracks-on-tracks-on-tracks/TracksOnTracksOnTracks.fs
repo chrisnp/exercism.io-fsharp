9618904b25082f5813dde86697f16c191f56022b
Invigo <invigo.devel@gmail.com>
2022-05-30 23:24:55 +0300

    Refactor TracksOnTracksOnTracks.fs

--- tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
+++ tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
@@ -4,19 +4,19 @@ let newList: string list =
     []
 
 let existingList: string list = 
-    ["Haskell"; "F#"; "Clojure"]
+    [ "F#"; "Clojure"; "Haskell" ]
 
 let addLanguage (language: string) (languages: string list): string list =
     language :: languages
 
 let countLanguages (languages: string list): int = 
-    languages 
-    |> List.length
+    languages |> List.length
 
 let reverseList(languages: string list): string list = 
     languages |> List.rev
 
 let excitingList (languages: string list): bool = 
     match languages with
-    | "F#" :: _ | [ _; "F#" ] | [ _; "F#"; _ ] -> true
-    | _ -> false
+    | "F#" :: _      -> true
+    | _ :: "F#" :: _ -> languages |> List.length <= 3
+    | _              -> false
