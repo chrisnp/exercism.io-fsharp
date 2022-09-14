e3f9b2c0d6cf4ec75944da9414d6201a30a95269
Invigo <invigo.devel@gmail.com>
2022-09-14 21:52:28 +0300

    NthPrime.fs, 1st passing iteration

--- nth-prime/NthPrime.fs
+++ nth-prime/NthPrime.fs
@@ -1,3 +1,32 @@
 module NthPrime
 
-let prime nth : int option = failwith "You need to implement this function."
+open System.Collections.Generic
+
+// Reusing Sieve.fs code
+let rec sieve limit = 
+    seq { 
+        yield 2
+        let composites = new HashSet<int>()
+        for i in 3..2..limit do
+            let found = composites.Contains i
+            if not found then
+                yield i
+            do for j in i..i..limit do 
+                composites.Add j |> ignore 
+    }
+
+let primes limit = 
+        match limit with
+            | 0 | 1 -> []
+            | _     -> sieve limit |> Seq.toList
+
+let prime nth : int option = 
+    let theoreticalLimit p : int =
+        let n = float p 
+        (2. + (((4. / 3.) * n * System.Math.Log(n |> float, 2.)))) 
+        |> int
+    if nth <= 0 then 
+        None
+    else 
+        primes (theoreticalLimit <| nth) |> Seq.item (nth - 1) 
+        |> Some
