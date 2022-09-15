ed289ff0ca476585175b2f32633d9803657b6521
Invigo <invigo.devel@gmail.com>
2022-09-15 20:47:25 +0300

    Update PizzaPricing.fs

--- pizza-pricing/PizzaPricing.fs
+++ pizza-pricing/PizzaPricing.fs
@@ -1,7 +1,26 @@
 module PizzaPricing
 
-// TODO: please define the 'Pizza' discriminated union type
+type Pizza = 
+    | Caprese
+    | Margherita
+    | Formaggio
+    | ExtraSauce of Pizza
+    | ExtraToppings of Pizza
 
-let pizzaPrice (pizza: Pizza): int = failwith "Please implement the 'pizzaPrice' function"
+let rec pizzaPrice (pizza: Pizza): int = 
+    match pizza with
+    | Margherita      -> 7
+    | Caprese         -> 9
+    | Formaggio       -> 10
+    | ExtraSauce p    -> 1 + (pizzaPrice <| p)
+    | ExtraToppings p -> 2 + (pizzaPrice <| p)
 
-let orderPrice(pizzas: Pizza list): int = failwith "Please implement the 'orderPrice' function"
+let orderPrice(pizzas: Pizza list): int =
+    let basePrice = pizzas |> List.sumBy pizzaPrice
+    let extraCharge = 
+        match pizzas |> List.length with
+        | 1 -> 3
+        | 2 -> 2
+        | _ -> 0
+    basePrice + extraCharge
+    
