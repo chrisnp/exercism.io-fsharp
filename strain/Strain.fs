e61db5e1c846d69bb2c35fe0c9b839028c06761a
Invigo <invigo.devel@gmail.com>
2022-09-16 23:36:18 +0300

    Reformat Strain.fs

--- strain/Strain.fs
+++ strain/Strain.fs
@@ -1,8 +1,5 @@
 ﻿module Seq
 
-let rec keep pred xs = 
-    [for x in xs do 
-        if x |> pred then yield x ]
+let rec keep pred xs = [for x in xs do if x |> pred then yield x ]
 
-let discard pred xs = 
-    keep (not << pred) xs
+let discard pred xs = keep (not << pred) xs
