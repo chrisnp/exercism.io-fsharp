d779d92fa91320101f3c17ebad9af90e267cbaea
Invigo <invigo.devel@gmail.com>
2022-09-16 23:38:32 +0300

    Refactor RotationalCipher.fs

--- rotational-cipher/RotationalCipher.fs
+++ rotational-cipher/RotationalCipher.fs
@@ -2,14 +2,16 @@
 
 open System
 
-let rot26 key ch =
+let rot26 (key, ch) =
+    let A = int 'A'
+    let a = int 'a'
     match ch with   
-    | ch when (ch |> Char.IsUpper) -> (int ch + key - int 'A') % 26 + (int 'A') |> char
-    | ch when (ch |> Char.IsLower) -> (int ch + key - int 'a') % 26 + (int 'a') |> char
+    | ch when (ch |> Char.IsUpper) -> A + (int ch + key - A) % 26 |> char
+    | ch when (ch |> Char.IsLower) -> a + (int ch + key - a) % 26 |> char
     | ch when (ch |> Char.IsLetter |> not) -> ch
     | _ -> failwith "Error"
 
 let rotate shiftKey text = 
     text
-    |> Seq.map (fun x -> rot26 shiftKey x)
+    |> Seq.map (fun x -> (shiftKey, x) |> rot26)
     |> String.Concat
