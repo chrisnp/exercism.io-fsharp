b98cf6ceed875f556adec64d47e79d840a64750d
Invigo <invigo.devel@gmail.com>
2022-09-29 01:05:26 +0300

    Create Allergies.fs

---
+++ allergies/Allergies.fs
@@ -0,0 +1,21 @@
+module Allergies
+
+open System
+
+type Allergen = 
+    | Eggs         = 0x01
+    | Peanuts      = 0x02
+    | Shellfish    = 0x04
+    | Strawberries = 0x08
+    | Tomatoes     = 0x10
+    | Chocolate    = 0x20
+    | Pollen       = 0x40
+    | Cats         = 0x80
+
+let allergicTo codedAllergies allergen = 
+    enum<Allergen> codedAllergies &&& allergen <> allergen
+
+let list codedAllergies = 
+    Enum.GetValues<Allergen>()
+    |> Seq.filter (allergicTo codedAllergies)
+    |> Seq.toList
