6f5bcb129af8dbf1cc84f0a9d079f396f2cbf3d3
Invigo <invigo.devel@gmail.com>
2022-12-05 17:58:39 +0200

    Refactor dnd-character

--- dnd-character/DndCharacter.fs
+++ dnd-character/DndCharacter.fs
@@ -1,35 +1,23 @@
 ﻿module DndCharacter
 
+let modifier x = float (x - 10) / 2.0 |> System.Math.Floor |> int
 
-let modifier x =
-    float (x - 10) / 2.0 
-    |> System.Math.Floor 
-    |> int
+let ability() = List.init 4 (fun _ -> System.Random().Next(6) + 1) 
+                |> List.skip 1 |> List.sum
 
-let ability() = 
-    List.init 4 (fun _ -> 
-                    System.Random()
-                          .Next(6) + 1)
-    |> List.skip 1 
-    |> List.sum
+type Character() =
+    let strength: int = ability()
+    let dexterity: int = ability()
+    let constitution: int = ability()
+    let intelligence: int = ability()
+    let wisdom: int = ability()
+    let charisma: int = ability()
+    member __.Strength with get() = strength
+    member __.Dexterity with get() =  dexterity
+    member __.Constitution with get() = constitution
+    member __.Intelligence with get() = intelligence
+    member __.Wisdom with get() = wisdom
+    member __.Charisma with get() = charisma
+    member __.Hitpoints with get() = modifier(__.Constitution) + 10
 
-type Character =
-    { Strength: int
-      Dexterity: int
-      Constitution: int
-      Intelligence: int
-      Wisdom: int
-      Charisma: int }
-    member this.Hitpoints = 
-        modifier (this.Constitution) + 10
-    
-    
-let createCharacter() : Character =  
-    { Strength = ability ()
-      Dexterity = ability ()
-      Constitution = ability ()
-      Intelligence = ability ()
-      Wisdom = ability ()
-      Charisma = ability () }
-    
-
+let createCharacter() = Character()
