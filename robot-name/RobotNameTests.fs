37d5d1e0b69739b18c94af404a1b6ba7ff5cfc7c
Invigo <invigo.devel@gmail.com>
2022-12-05 18:03:26 +0200

    Update RobotNameTests.fs

--- robot-name/RobotNameTests.fs
+++ robot-name/RobotNameTests.fs
@@ -12,18 +12,28 @@ let ``Robot has a name`` () =
     let robot = mkRobot()
     Regex.IsMatch(name robot, @"^[A-Z]{2}\d{3}$") |> should equal true
     
-[<Fact>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Name is the same each time`` () =     
     let robot = mkRobot()
     name robot |> should equal (name robot)
     
-[<Fact>]
-let ``Different robots have different names`` () = 
+[<Fact(Skip = "Remove this Skip property to run this test")>]
+let ``2 Different robots have different names`` () = 
     let robot = mkRobot()
     let robot2 = mkRobot()
     name robot |> should not' (equal (name robot2))
+
+[<Fact(Skip = "Remove this Skip property to run this test")>] 
+let ``2500 Different robots have different names``() =
+    let robotCount = 2500
+    seq { 1 .. robotCount }
+    |> Seq.map (fun _ -> mkRobot())
+    |> Seq.map (fun robot -> name robot)
+    |> Set
+    |> Set.count
+    |> should equal robotCount
     
-[<Fact>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Can reset the name`` () =  
     let robot = mkRobot()
     let originalName = name robot
