a42d4262450d63eea68b9914e1d1b133135ced67
Invigo <invigo.devel@gmail.com>
2022-12-07 22:23:29 +0200

    Reformat Proverb.fs

--- proverb/Proverb.fs
+++ proverb/Proverb.fs
@@ -4,14 +4,14 @@ let recite (input: string list): string list =
     match input with
     | []  -> []
     | [x] -> [ sprintf "And all for the want of a %s." x ]
-    | _   -> seq { 
-                yield! 
-                    input 
-                    |> List.pairwise 
-                    |> List.map(fun (x, y) -> 
-                                sprintf "For want of a %s the %s was lost." 
-                                <|| (x, y))
-                yield  
-                    sprintf "And all for the want of a %s." input.Head 
-             } 
-             |> Seq.toList
+    | _   -> 
+        seq { 
+            yield! 
+                input 
+                |> List.pairwise 
+                |> List.map(fun (x, y) -> 
+                    sprintf "For want of a %s the %s was lost." <|| (x, y))
+            yield  
+                sprintf "And all for the want of a %s." input.Head 
+            } 
+        |> Seq.toList
