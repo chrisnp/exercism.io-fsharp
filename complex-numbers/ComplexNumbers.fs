1b5053fdc23a7c9ba992326a0f2b4fb9c18d6838
Invigo <invigo.devel@gmail.com>
2022-12-09 03:11:01 +0200

    Update ComplexNumbers.fs

--- complex-numbers/ComplexNumbers.fs
+++ complex-numbers/ComplexNumbers.fs
@@ -1,21 +1,40 @@
 module ComplexNumbers
 
-let create real imaginary = failwith "You need to implement this function."
+type ComplexNumber = {re: float; im: float}
 
-let mul z1 z2 = failwith "You need to implement this function."
+let create real imaginary = { re = real; im = imaginary} 
 
-let add z1 z2 = failwith "You need to implement this function."
+let mul z1 z2 = 
+    let real = z1.re * z2.re - z1.im * z2.im
+    let imag = z1.im * z2.re + z1.re * z2.im
+    create real imag
 
-let sub z1 z2 = failwith "You need to implement this function."
+let add z1 z2 = 
+    let real = z1.re + z2.re
+    let imag = z1.im + z2.im
+    create real imag
 
-let div z1 z2 = failwith "You need to implement this function."
+let sub z1 z2 = 
+    let real = z1.re - z2.re
+    let imag = z1.im - z2.im
+    create real imag
 
-let abs z = failwith "You need to implement this function."
+let div z1 z2 = 
+    let quot = z2.re ** 2.0 + z2.im ** 2.0
+    let real = (z1.re * z2.re + z1.im * z2.im) / quot
+    let imag = (z1.im * z2.re - z1.re * z2.im) / quot
+    create real imag
 
-let conjugate z = failwith "You need to implement this function."
+let abs z = ( z.re ** 2.0 + z.im ** 2.0 ) ** 0.5
 
-let real z = failwith "You need to implement this function."
+let conjugate z = create z.re -z.im
 
-let imaginary z = failwith "You need to implement this function."
+let real z = z.re
 
-let exp z = failwith "You need to implement this function."
+let imaginary z = z.im
+
+let exp z = 
+    let xp   = z.re |> System.Math.Exp
+    let real = xp * (z.im |> System.Math.Cos) 
+    let imag = xp * (z.im |> System.Math.Sin) 
+    create real imag
