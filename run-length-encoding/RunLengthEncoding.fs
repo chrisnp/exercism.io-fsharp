de29747a273fc6955bf7827bbd30310bc23c3639
Invigo <invigo.devel@gmail.com>
2023-08-28 23:23:42 +0300

    RunLengthEncoding, 1st passing iteration.

--- run-length-encoding/RunLengthEncoding.fs
+++ run-length-encoding/RunLengthEncoding.fs
@@ -1,5 +1,37 @@
 module RunLengthEncoding
 
-let encode input = failwith "You need to implement this function."
+open System
 
-let decode input = failwith "You need to implement this function."
+let private (|Count|_|) (str: String) =
+    if str |> String.length = 0 then 
+        None
+    else 
+        let len = str |> Seq.takeWhile((=) str.[0]) 
+                      |> Seq.length
+        Some (str.Substring(0, 1), len, str.[len..]) 
+
+let rec encode input = 
+    let emit times letter =
+        match times with
+        | 1 -> letter
+        | x -> String.Format ("{0}{1}", x, letter)
+    match input with
+    | "" -> ""
+    | Count (n, c, rest) -> (emit c n) + (encode rest) 
+    | _ -> failwith "Here to prevent warning FS0025 (by preserving tail recursion)"
+
+let private (|Digits|_|) (str: String)= 
+    let dsMatch = 
+        Text.RegularExpressions.Regex.Match(str, "^([0-9]+)?([\w\s])")
+    match dsMatch.Success, dsMatch.Groups.[1].Value with
+    | true, "" -> 
+        Some (1, char dsMatch.Groups.[2].Value, str[dsMatch.Length..])
+    | true, n ->
+        Some (int n, char dsMatch.Groups.[2].Value, str[dsMatch.Length..])
+    | false, _ -> None
+
+let rec decode input = 
+    match input with
+    | "" -> ""
+    | Digits (n, c, rest) -> String(c, n) + (decode rest)
+    | _ -> failwith "Here to prevent warning FS0025 (by preserving tail recursion)"
