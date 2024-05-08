2b37e476d6d169689ed51884b596a059b1b34893
Invigo <invigo.devel@gmail.com>
2024-05-08 15:09:47 +0300

    Update BookingUpForBeauty.fs

--- booking-up-for-beauty/BookingUpForBeauty.fs
+++ booking-up-for-beauty/BookingUpForBeauty.fs
@@ -1,20 +1,19 @@
 module BookingUpForBeauty
 
-// The following line is needed to use the DateTime type
 open System
 
-let schedule = DateTime.Parse    
+let schedule = DateTime.Parse 
 
 let hasPassed = (>) DateTime.Now
 
 let isAfternoonAppointment (appointmentDate: DateTime): bool =
     let
-        time = appointmentDate.Hour 
-    in
+        time = appointmentDate.Hour
+    in 
         (&&) ((<=) 12 <| time) ((>) 18 <| time)
 
-let description (appointmentDate: DateTime): string  = 
+let description (appointmentDate: DateTime): string = 
     sprintf <| "You have an appointment on %A." <| appointmentDate
 
 let anniversaryDate(): DateTime = 
-    new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0)
+    new DateTime (DateTime.Now.Year, 9, 15, 0, 0, 0)
