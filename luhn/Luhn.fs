5899aed181ec9002874b2fb567f42933c4c5f028
Invigo <invigo.devel@gmail.com>
2024-05-30 19:55:19 +0300

    Luhn.fs - passing all tests

--- luhn/Luhn.fs
+++ luhn/Luhn.fs
@@ -1,3 +1,23 @@
 module Luhn
 
-let valid number = failwith "You need to implement this function."
+open System
+
+let private (<|>) f x y = f y x 
+
+let rec private DoubleEvens acc = 
+    let f x = let y = 2 * x in if x < 5 then y else y - 9
+    function
+    | []           -> acc
+    | [x]          -> x :: acc
+    | x :: y :: xs -> DoubleEvens (f y :: x :: acc) xs
+
+
+let valid number = 
+    let sanitary = number |> String.filter (Char.IsWhiteSpace >> not)
+    match sanitary.Length <= 1 || 
+          sanitary |> String.forall Char.IsDigit |> not
+    with
+    | true -> false
+    | _ -> sanitary |> Seq.map (Char.GetNumericValue >> int)
+           |> Seq.rev |> List.ofSeq |> DoubleEvens [] |> List.sum
+           |> (<|>) (%) 10 |> (=) 0
