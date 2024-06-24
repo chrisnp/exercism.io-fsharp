dd33d9c399ab46148e4b9e9c2df73a2e7288a28d
Invigo <invigo.devel@gmail.com>
2024-06-24 16:43:07 +0300

    Update SqueakyClean.fs

--- squeaky-clean/SqueakyClean.fs
+++ squeaky-clean/SqueakyClean.fs
@@ -2,8 +2,17 @@ module SqueakyClean
 
 open System
 
+let private IsNotGreek (x: char): bool = x < 'α' || 'ω' < x
+
 let transform (c: char) : string =
-    failwith "Please implement the 'transform' function"
+    if IsNotGreek(c) then
+        if c.Equals('-') then "_"
+        else if Char.IsWhiteSpace(c) then ""
+        else if Char.IsUpper(c) then "-" + c.ToString().ToLower()
+        else if Char.IsDigit(c) then ""
+        else c.ToString()
+    else "?"
     
 let clean (identifier: string): string =
-    failwith "Please implement the 'clean' function"
+    identifier 
+    |> String.collect transform
