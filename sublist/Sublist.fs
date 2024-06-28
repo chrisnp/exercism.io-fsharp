3fd497093b10ad1d9545c7ff4bc7edec4fcb7afc
Invigo <invigo.devel@gmail.com>
2024-06-28 15:54:19 +0300

    Create Sublist.fs

---
+++ sublist/Sublist.fs
@@ -0,0 +1,18 @@
+module Sublist
+
+type SublistType = Equal | Sublist | Superlist | Unequal
+
+let private isSublist (xs: 'a list) (ys: 'a list): bool =
+    ys = [] || 
+    (
+    xs 
+    |> List.windowed (max 1 (min xs.Length ys.Length))
+    |> List.contains ys
+    )
+
+let sublist (xs: 'a list) (ys: 'a list): SublistType = 
+    match (isSublist ys xs, isSublist xs ys) with
+    | (true, true)  -> Equal
+    | (true, false) -> Sublist
+    | (false, true) -> Superlist
+    | (_, _)        -> Unequal
