d0bafc3813424c5dd8080437ea3c2e55d7a284f4
Invigo <invigo.devel@gmail.com>
2024-09-12 23:00:20 +0300

    Update MatchingBrackets.fs

--- matching-brackets/MatchingBrackets.fs
+++ matching-brackets/MatchingBrackets.fs
@@ -1,3 +1,18 @@
 module MatchingBrackets
 
-let isPaired input = failwith "You need to implement this function."
+let brackets = ['[';'{';'(';')';'}';']']
+
+let isPaired (input: string): bool = 
+    let rec paired bs stack =
+        match bs, stack with
+        // | _ :: _ -> false
+        | ('('|'['|'{')::xs, stack -> paired xs (Seq.head xs)::stack
+        | ')'::xs, '('::ys -> paired xs ys
+        | '}'::xs, '{'::ys -> paired xs ys
+        | ']'::xs, '['::ys -> paired xs ys
+        | [], [] -> true
+        | _, _ -> false
+    input 
+    |> Seq.toList 
+    |> List.filter (fun c -> brackets |> List.contains c)
+    |> paired []
