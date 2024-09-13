a2aa2732b6e3fc547fd4bf56c201a8886335e415
Invigo <invigo.devel@gmail.com>
2024-09-13 22:22:36 +0300

    MatchingBrackets.fs - Passing all tests

--- matching-brackets/MatchingBrackets.fs
+++ matching-brackets/MatchingBrackets.fs
@@ -1,18 +1,16 @@
 module MatchingBrackets
 
-let brackets = ['[';'{';'(';')';'}';']']
-
 let isPaired (input: string): bool = 
-    let rec paired bs stack =
-        match bs, stack with
-        // | _ :: _ -> false
-        | ('('|'['|'{')::xs, stack -> paired xs (Seq.head xs)::stack
-        | ')'::xs, '('::ys -> paired xs ys
-        | '}'::xs, '{'::ys -> paired xs ys
-        | ']'::xs, '['::ys -> paired xs ys
-        | [], [] -> true
-        | _, _ -> false
-    input 
-    |> Seq.toList 
-    |> List.filter (fun c -> brackets |> List.contains c)
-    |> paired []
+    let rec paired (bs: char list) (stack: char list): bool =
+        match (bs, stack) with
+        | ([], ys)      -> ys |> List.isEmpty
+        | ('('::xs, ys) -> paired (xs) ('('::ys)
+        | ('{'::xs, ys) -> paired (xs) ('{'::ys)
+        | ('['::xs, ys) -> paired (xs) ('['::ys)
+        | (')'::xs, '('::ys) 
+        | ('}'::xs, '{'::ys) 
+        | (']'::xs, '['::ys) -> paired xs ys
+        | ((')'|'}'|']')::xs, []) 
+        | ((')'|'}'|']')::xs, _)  -> false
+        | _::xs, ys -> paired xs ys
+    paired (input |> Seq.toList) [] 
