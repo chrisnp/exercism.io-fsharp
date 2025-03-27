8359159d8e15daf2d19607d2af2f117ce0697491
Invigo <invigo.devel@gmail.com>
2025-03-27 22:51:18 +0200

    Reformat MatchingBrackets

--- matching-brackets/MatchingBrackets.fs
+++ matching-brackets/MatchingBrackets.fs
@@ -3,14 +3,14 @@ module MatchingBrackets
 let isPaired (input: string): bool = 
     let rec paired (brackets: char list) (stack: char list): bool =
         match (brackets, stack) with
-        | ([], ys)      -> ys |> List.isEmpty
-        | ('('::xs, ys) -> paired (xs) ('('::ys)
-        | ('{'::xs, ys) -> paired (xs) ('{'::ys)
-        | ('['::xs, ys) -> paired (xs) ('['::ys)
+        | ([], ys)                -> ys |> List.isEmpty
+        | ('('::xs, ys)           -> paired (xs) ('('::ys)
+        | ('{'::xs, ys)           -> paired (xs) ('{'::ys)
+        | ('['::xs, ys)           -> paired (xs) ('['::ys)
         | (')'::xs, '('::ys) 
         | ('}'::xs, '{'::ys) 
-        | (']'::xs, '['::ys) -> paired xs ys
+        | (']'::xs, '['::ys)      -> paired xs ys
         | ((')'|'}'|']')::xs, []) 
         | ((')'|'}'|']')::xs, _)  -> false
-        | _::xs, ys -> paired xs ys
+        | _::xs, ys               -> paired xs ys
     paired (input |> Seq.toList) [] 
