751eaa8d4377fde8e7c2d3d3340f19f2cadab0b3
Invigo <invigo.devel@gmail.com>
2025-03-28 18:35:57 +0200

    Update ResistorColorTests.fs

--- resistor-color/ResistorColorTests.fs
+++ resistor-color/ResistorColorTests.fs
@@ -9,15 +9,15 @@ open ResistorColor
 let ``Black`` () =
     colorCode "black" |> should equal 0
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``White`` () =
     colorCode "white" |> should equal 9
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Orange`` () =
     colorCode "orange" |> should equal 3
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Colors`` () =
     colors |> should equal ["black"; "brown"; "red"; "orange"; "yellow"; "green"; "blue"; "violet"; "grey"; "white"]
 
