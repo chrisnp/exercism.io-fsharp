5d1414c3fd1fdb5b244ef96fdc4889bf72ef75a3
Invigo <invigo.devel@gmail.com>
2025-04-02 01:23:34 +0300

    Refactor PascalsTriangle.fs

--- pascals-triangle/PascalsTriangle.fs
+++ pascals-triangle/PascalsTriangle.fs
@@ -1,13 +1,13 @@
 module PascalsTriangle
 
 let rows numberOfRows : int list list = 
-    let binomial x y = List.fold (fun r i -> r * (x - i + 1) / i ) 1 [1..y]
-    // if numberOfRows < 0 then 
-    //     failwith "Nr of rows cannot be less than zero"
-    seq {for x in 0..(numberOfRows - 1) do
-            yield seq {for y in 0..x do
-                            yield binomial x y
-                      } 
-            |> List.ofSeq
-        } 
-        |> List.ofSeq       
+    let rec binomialc n k = 
+        if k = n || k = 0 then 1 
+        else (binomialc (n - 1) (k - 1)) + (binomialc (n - 1) k)
+    seq { for x in 0..(numberOfRows - 1) do
+            yield seq { for y in 0..x do
+                            yield binomialc x y
+                  } 
+                  |> List.ofSeq
+    }
+    |> List.ofSeq       
